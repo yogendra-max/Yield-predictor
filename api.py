1d199dbdf210d54dfc24746e38be4fba639152a6
yogendr-max <yogendra08008q@gmail.com>
2025-08-28 11:17:24 +0530

    Initial commit

--- api.py
+++ api.py
@@ -10,16 +10,17 @@ model = joblib.load("yield_model.pkl")
 @app.route('/predict', methods=['POST'])
 def predict_yield():
     try:
-        # Get JSON request
         data = request.get_json()
-        df = pd.DataFrame([data])  # Convert to DataFrame
+        if not data:
+            return jsonify({'error': 'No input data provided'}), 400
 
-        # Predict
+        df = pd.DataFrame([data])  # Convert JSON to DataFrame
         prediction = model.predict(df)[0]
+
         return jsonify({'Predicted_Yield': float(prediction)})
 
     except Exception as e:
-        return jsonify({'error': str(e)})
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
