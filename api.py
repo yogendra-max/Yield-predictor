20c424e473f2b2fdd11ece007180479aed5097f2
yogendr-max <yogendra08008q@gmail.com>
2025-08-29 10:46:51 +0530

    updated hosting

--- api.py
+++ api.py
@@ -56,4 +56,4 @@ def predict_yield():
 # -------------------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
